fa4abd602cdec0466b9e69331a12b9e2139a43b9
TallesAndrade <tallesandrade7@gmail.com>
2025-09-21 19:37:35 -0300

    fix:corrigi entidade no Estoque e adiciona extends no Venda

--- src/main/java/com/talles/armazemdamargarete/repository/EstoqueRepository.java
+++ src/main/java/com/talles/armazemdamargarete/repository/EstoqueRepository.java
@@ -1,9 +1,9 @@
 package com.talles.armazemdamargarete.repository;
 
-import com.talles.armazemdamargarete.entity.ProdutoVenda;
+import com.talles.armazemdamargarete.entity.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstoqueRepository extends JpaRepository<ProdutoVenda, Long> {
+public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
 }
--- src/main/java/com/talles/armazemdamargarete/repository/VendaRepository.java
+++ src/main/java/com/talles/armazemdamargarete/repository/VendaRepository.java
@@ -1,7 +1,9 @@
 package com.talles.armazemdamargarete.repository;
 
+import com.talles.armazemdamargarete.entity.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VendaRepository {
+public interface VendaRepository extends JpaRepository<Venda, Long> {
 }
